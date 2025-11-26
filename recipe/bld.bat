71f8628540c42fe2ee3357af9d3690dec3cff286
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-25 19:44:25 -0800

    use pixi

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,10 @@
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" .\cmd\f2 || goto :error
+@echo on
+@setlocal EnableDelayedExpansion
+
+go build -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" .\cmd\f2 || goto :error
 go-licenses save .\cmd\f2 --save_path=license-files || goto :error
 
-goto :EOF
+goto :eof
 
 :error
 echo Failed with error #%errorlevel%.
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,5 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w" ./cmd/f2
+go build -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w" ./cmd/f2
 go-licenses save ./cmd/f2 --save_path=license-files
